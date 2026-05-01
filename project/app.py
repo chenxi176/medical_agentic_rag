303e2f067b45447427bbe84dc20f059d9db9e8c7
chenxi176 <115556388+chenxi176@users.noreply.github.com>
2026-05-01 23:33:47 +0800

    Add files via upload
    增加MCP工具
    Agent自身的训练数据可能有滞后或不完整，通过MCP接入专业医学知识库API，可以：
    • 查询最新诊疗指南：实时获取特定疾病（如高血压、糖尿病）的最新国内外临床指南（NCCN、ESC等）。
    • 检索药品说明书：精准返回某种药物的适应症、用法用量、禁忌症、不良反应（比通用模型更准确）。

--- project/app.py
+++ project/app.py
@@ -22,10 +22,22 @@ from ui.gradio_app import create_gradio_ui
 import os
 os.environ['TESSDATA_PREFIX'] = '/usr/share/tesseract-ocr/4.00/tessdata/'
 
+#add
+import atexit
+import signal
+import sys, asyncio
+
+from my_mcp_server import cleanup_mcp
+
+atexit.register(cleanup_mcp)
+signal.signal(signal.SIGINT, lambda s, f: sys.exit(0))
+signal.signal(signal.SIGTERM, lambda s, f: sys.exit(0))
+# 删除原有的 global_mcp_client 定义及其他 asyncio.run 调用
+#add end
+
 import gradio as gr
 if __name__ == "__main__":
     print("\n🔨 Creating RAG Assistant...")
     demo = create_gradio_ui()
     print("\n🚀 Launching RAG Assistant...")
     demo.launch(theme=gr.themes.Citrus(),server_name="0.0.0.0",server_port=6006,share=False)
-    #!lsof -i:6006css=custom_css
